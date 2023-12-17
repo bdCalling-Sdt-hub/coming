08f8565352508b8a617a3ee1577725ea0fa9086d
siffahim <siffahim25@gmail.com>
2023-12-17 13:05:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import img from "./assets/bdcallingacademy.com.gif";
+
+function App() {
+  return (
+    <>
+      <img src={img} alt="" />
+    </>
+  );
+}
+
+export default App;
